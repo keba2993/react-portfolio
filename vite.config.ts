7c8d57b91b6d2e8d3eaf4323fa04998794363da0
Kevin Barone <72464586+keba2993@users.noreply.github.com>
2025-03-21 16:52:22 -0600

    specify build options

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	root: './',
+	build: {
+		outDir: '../dist',
+		assetsDir: './src/assets',
+		emptyOutDir: true,
+	},
 });

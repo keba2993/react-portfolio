f1d09a49c193bd061fef41b57e364adea78cf965
Kevin Barone <72464586+keba2993@users.noreply.github.com>
2025-03-24 13:37:13 -0600

    remove css files, mobile first

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import './App.css';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Experience from './components/Experience/Experience';
 import Skills from './components/Skills/Skills';
-import { ThemeProvider, createTheme } from '@mui/material';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 
 const theme = createTheme({
 	palette: {
@@ -21,6 +20,7 @@ function App() {
 	return (
 		<>
 			<ThemeProvider theme={theme}>
+				<CssBaseline />
 				<Navbar />
 				<Hero />
 				<Experience />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,28 @@
-import { Typography, IconButton } from '@mui/material';
+import { Typography, IconButton, Container } from '@mui/material';
 import { GitHub, LinkedIn } from '@mui/icons-material';
 import Grid from '@mui/material/Grid2';
-// import portraitImg from '../assets/portrait-nasdaq.jpg';
 
 function Hero() {
 	return (
-		<Grid
-			container
-			spacing={4}
-			className='mx-auto max-w-screen-xl px-6 md:px-12 lg:px-24 lg:py-6'
-		>
+		<Grid container spacing={{ xs: 1, sm: 2, md: 4, lg: 4 }} direction='row'>
 			<Grid
-				container
-				spacing={2}
-				size={{ xs: 12, sm: 12, md: 12 }}
-				direction='row'
-				sx={{ maxHeight: '100%' }}
+				size={{ xs: 12, sm: 12, md: 6, lg: 4 }}
+				sx={{
+					display: 'flex',
+					justifyContent: 'center',
+					flexDirection: 'column',
+					padding: '1rem 1rem 0 1rem',
+				}}
 			>
-				<Grid size={4}>
-					<Typography variant='h2'>Kevin Barone</Typography>
-					<Typography variant='h5'>Fullstack Software Engineer</Typography>
-
+				<Typography variant='h2'>Kevin Barone</Typography>
+				<Typography variant='h5'>Fullstack Software Engineer</Typography>
+				<Container
+					sx={{
+						display: 'flex',
+						alignItems: 'center',
+						padding: '0px',
+					}}
+				>
 					<IconButton
 						size='large'
 						color='inherit'
@@ -40,41 +42,33 @@ function Hero() {
 					>
 						<LinkedIn />
 					</IconButton>
-				</Grid>
-				<Grid size={8}>
-					<Typography variant='body1' align='justify'>
-						I am a fullstack developer with a passion for building robust web
-						applications and empowering software teams to develop impactful
-						products. I have experience working with a variety of technologies
-						including React, Node.js, TypeScript, and Java. I am always looking
-						for new opportunities to learn and grow as a developer by working on
-						challenging projects. <br />
-						<br />
-						Growing up, I first discoverd my interest in programming while
-						watching my father work as a software engineer. I was finally able
-						to take my first Computer Basics cours in 6th grade. I quickly fell
-						in love with technology and always wanted to find new ways be
-						creative with technoligical platforms.
-						<br />
-						<br />
-						Through internships I gained experience in a diverse range of
-						development environments, including full-stack web development,
-						mobile development, and test engineering. I continue to push myself
-						beyond my limits, learning new technologies and frameworks to expand
-						my skillset. Taking ownership of my work and being a team leader has
-						always been a priority for me as I continue to grow my career in
-						software development.
-					</Typography>
-				</Grid>
+				</Container>
 			</Grid>
-			{/* <Grid size={{ xs: 12, sm: 12, md: 4 }} sx={{ textAlign: 'center' }}>
-				<Box
-					component='img'
-					className='hero-img'
-					alt='Kevin Barone Portrait Image'
-					src={portraitImg}
-				/>
-			</Grid> */}
+			<Grid size={{ xs: 12, sm: 12, md: 6, lg: 8 }} sx={{ padding: '1rem' }}>
+				<Typography variant='body1' align='justify'>
+					I am a fullstack developer with a passion for building robust web
+					applications and empowering software teams to develop impactful
+					products. I have experience working with a variety of technologies
+					including React, Node.js, TypeScript, and Java. I am always looking
+					for new opportunities to learn and grow as a developer by working on
+					challenging projects. <br />
+					<br />
+					Growing up, I first discoverd my interest in programming while
+					watching my father work as a software engineer. I was finally able to
+					take my first Computer Basics cours in 6th grade. I quickly fell in
+					love with technology and always wanted to find new ways be creative
+					with technoligical platforms.
+					<br />
+					<br />
+					Through internships I gained experience in a diverse range of
+					development environments, including full-stack web development, mobile
+					development, and test engineering. I continue to push myself beyond my
+					limits, learning new technologies and frameworks to expand my
+					skillset. Taking ownership of my work and being a team leader has
+					always been a priority for me as I continue to grow my career in
+					software development.
+				</Typography>
+			</Grid>
 		</Grid>
 	);
 }

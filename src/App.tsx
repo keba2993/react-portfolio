39e4f68a8c5061740ae2b907c02b90b348692513
Kevin Barone <72464586+keba2993@users.noreply.github.com>
2025-03-24 19:43:18 -0600

    adding a custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,16 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Experience from './components/Experience/Experience';
 import Skills from './components/Skills/Skills';
-import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
-
-const theme = createTheme({
-	palette: {
-		mode: 'light',
-		primary: {
-			main: '#557246',
-		},
-		secondary: {
-			main: '#ab6ab9',
-		},
-	},
-});
+import { Paper } from '@mui/material';
 
 function App() {
 	return (
-		<>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<Navbar />
-				<Hero />
-				{/* <Experience />
-				<Skills /> */}
-			</ThemeProvider>
-		</>
+		<Paper>
+			<Navbar />
+			<Hero />
+			<Experience />
+			<Skills />
+		</Paper>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+
+const theme = createTheme({
+	palette: {
+		mode: 'dark',
+		primary: {
+			main: '#a7cf75',
+			dark: '#557246',
+			light: '#c1de9e',
+		},
+		secondary: {
+			main: '#395e7d',
+			dark: '#13338d',
+			light: '#a4c1df',
+		},
+		text: {
+			primary: '#efefef',
+			secondary: '#e2e2e2',
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
